6710b17103b70ebaba2667afe1a108cb5c2699d9
Moisés <moysantos2013@gmail.com>
2020-08-31 00:35:52 -0600

    primer commit, ya busca imagenes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Formulario from "./components/Formulario";
+import ListadoImagenes from "./components/ListadoImagenes";
 
 function App() {
+
+  const [busqueda, setBusqueda] = useState('');
+  const [imagenes, setImagenes] = useState([]);
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      if (busqueda === '') return;
+
+      const imagenesPorPagina = 30;
+      const key = '18111963-313df546a17bd068802ac69b1';
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      setImagenes(resultado.hits);
+    }
+    consultarApi();
+
+  }, [busqueda])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de imágenes</p>
+        <Formulario
+          setBusqueda={setBusqueda}
+        />
+      </div>
+
+      <div className="row justify-content-center">
+        <ListadoImagenes imagenes={imagenes}/>
+      </div>
     </div>
   );
 }
